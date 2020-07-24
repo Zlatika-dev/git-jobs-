4626bfe37101aa724e34b8f68752c2fefb299b33
Zlatika <zlata@ellery.ru>
2020-07-24 17:19:58 +0300

    starting form

--- src/Job.js
+++ src/Job.js
@@ -1,10 +1,12 @@
-import React from 'react';
-import { Card, Badge } from 'react-bootstrap';
-import ReactMarkDown from 'react-markdown'
+import React, {useState} from 'react';
+import { Card, Badge, Button, Collapse } from 'react-bootstrap';
+import ReactMarkdown from 'react-markdown';
 
 export default function Job({ job }) {
+  const [open, setOpen] = useState(false)
+
   return (
-    <Card>
+    <Card className="mb-3">
       <Card.Body>
         <div className="d-flex justify-content-between">
           <div>
@@ -16,8 +18,25 @@ export default function Job({ job }) {
             </Card.Subtitle>
             <Badge variant="secondary" className="mr-2">{job.type}</Badge>
             <Badge variant="secondary">{job.location}</Badge>
+            <div style={{ wordBreak: 'break-all' }}>
+              <ReactMarkdown source={job.how_to_apply} />
+            </div>
           </div>
+          <img className="d-none d-md-block"
+            height="50"
+            alt={job.company}
+            src={job.company_logo} />
         </div>
+        <Card.Text>
+          <Button 
+          onClick={()=>setOpen(prevOpen => !prevOpen)} 
+          variant="primary">{open ? 'Hide Details' : 'View Details'}</Button>
+        </Card.Text>
+        <Collapse in={open}>
+          <div className="mt-4">
+            <ReactMarkdown source={job.description} />
+          </div>
+        </Collapse>
       </Card.Body>
 
     </Card>
